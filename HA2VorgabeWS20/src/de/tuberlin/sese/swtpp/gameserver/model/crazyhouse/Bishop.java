388c8b947fdda526a4ec8ae4d6475bbccd5b6430
Rheinix <52194391+Rheinix@users.noreply.github.com>
2021-02-12 23:22:52 +0100

    bishop und queen mit auf dem weg

--- HA2VorgabeWS20/src/de/tuberlin/sese/swtpp/gameserver/model/crazyhouse/Bishop.java
+++ HA2VorgabeWS20/src/de/tuberlin/sese/swtpp/gameserver/model/crazyhouse/Bishop.java
@@ -19,10 +19,31 @@ public class Bishop {
 	
 	
 	public boolean canI() {
-		int diffy = (int)Math.sqrt(Math.pow(this.yTo - this.yFrom,2));
-		int diffx = (int)Math.sqrt(Math.pow(this.xTo - this.xFrom,2));
-		
-		return diffx == diffy;
+		int diffy = Math.abs(this.yTo - this.yFrom);
+		int diffx = Math.abs(this.xTo - this.xFrom);
+		if(diffx == diffy) {
+			int anfangY;
+			int endeY;
+			if(this.xTo - this.xFrom < 0) {
+				anfangY = Math.max(this.yFrom, this.yTo)-1;
+				endeY = Math.min(this.yFrom, this.yTo)+1;
+			}else {
+				endeY = Math.max(this.yFrom, this.yTo)+1;
+				anfangY = Math.min(this.yFrom, this.yTo)-1;
+			}
+			int anfangX = Math.min(this.xFrom, this.xTo)+1;
+			int endeX = Math.max(this.xFrom, this.xTo)-1;
+			int iter;
+			if(anfangY < endeY) iter = 1;
+			else iter = -1;
+			int j = anfangY;
+			for(int i = anfangX; i <= endeX; i++) {
+				if(this.spielfeld[i][j] != 0) return false;
+				j+=iter;
+			}
+			return true;
+		}
+		return false;
 	}
 
 }
--- HA2VorgabeWS20/src/de/tuberlin/sese/swtpp/gameserver/model/crazyhouse/Queen.java
+++ HA2VorgabeWS20/src/de/tuberlin/sese/swtpp/gameserver/model/crazyhouse/Queen.java
@@ -17,19 +17,56 @@ public class Queen {
 	}
 	
 	public boolean canI() {
-		int diffy = (int)Math.sqrt(Math.pow(this.yTo - this.yFrom,2));
-		int diffx = (int)Math.sqrt(Math.pow(this.xTo - this.xFrom,2));
+		int diffy = Math.abs(this.yTo - this.yFrom);
+		int diffx = Math.abs(this.xTo - this.xFrom);
 		
 		return (this.canIDiagonal(diffx, diffy) || this.canILine(diffx, diffy));	
 	}
 	
 	
 	public boolean canIDiagonal(int diffx, int diffy) {
-		return diffx == diffy;
+		if(diffx == diffy) {
+			int anfangY;
+			int endeY;
+			if(this.xTo - this.xFrom < 0) {
+				anfangY = Math.max(this.yFrom, this.yTo)-1;
+				endeY = Math.min(this.yFrom, this.yTo)+1;
+			}else {
+				endeY = Math.max(this.yFrom, this.yTo)+1;
+				anfangY = Math.min(this.yFrom, this.yTo)-1;
+			}
+			int anfangX = Math.min(this.xFrom, this.xTo)+1;
+			int endeX = Math.max(this.xFrom, this.xTo)-1;
+			int iter;
+			if(anfangY < endeY) iter = 1;
+			else iter = -1;
+			int j = anfangY;
+			for(int i = anfangX; i <= endeX; i++) {
+				if(this.spielfeld[i][j] != 0) return false;
+				j+=iter;
+			}
+			return true;
+		}
+		return false;
 	}
 	
 public boolean canILine(int diffx, int diffy) {
-		if(diffy > 0) return diffx == 0;
-		else return diffx > 0;
+		if(diffy > 0 && diffx == 0) {
+			int anfang = Math.min(this.yFrom,this.yTo)+1;
+			int ende = Math.max(this.yFrom,this.yTo)-1;
+			for(int i = anfang; i <= ende; i++) {
+				if(this.spielfeld[this.xFrom][i] != 0) return false; 
+			}
+			return true;
+		}
+		else if(diffy == 0 && diffx > 0) {
+			int anfang = Math.min(this.xFrom,this.xTo)+1;
+			int ende = Math.max(this.xFrom,this.xTo)-1;
+			for(int i = anfang; i <= ende; i++) {
+				if(this.spielfeld[i][this.yFrom] != 0) return false; 
+			}
+			return true;
+		}
+		return false;
 	}
 }
