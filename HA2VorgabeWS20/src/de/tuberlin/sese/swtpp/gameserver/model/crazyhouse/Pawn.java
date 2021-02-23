ec92defb42181a6e2cba8453dfc928a779edddaf
Rheinix <52194391+Rheinix@users.noreply.github.com>
2021-02-23 10:04:25 +0100

    Pawn fix

--- HA2VorgabeWS20/src/de/tuberlin/sese/swtpp/gameserver/model/crazyhouse/Pawn.java
+++ HA2VorgabeWS20/src/de/tuberlin/sese/swtpp/gameserver/model/crazyhouse/Pawn.java
@@ -20,23 +20,27 @@ public class Pawn {
 		int ich = this.spielfeld[this.xFrom][this.yFrom];
 		int diffy = this.yTo - this.yFrom;
 		int diffx = this.xTo - this.xFrom;
-		if(ich <= 90) {			//weiss
-			if(Math.sqrt(Math.pow(diffx,2)) == 1 && diffy == 1) {//wir bewegen uns eins nach vorne und 1 links oder rechts
+		int offset1, offset2;
+		if(ich <= 90) {offset1 = 1; offset2 = 0;}//weiss
+		else {offset1 = -1; offset2 = 5;}
+
+		if(diffy == 1*offset1) {
+			if(Math.sqrt(Math.pow(diffx,2)) == 1) {//wir bewegen uns eins nach vorne und 1 links oder rechts
 				return (this.spielfeld[this.xTo][this.yTo] != 0);//ist dort ein gegner?
 			}
-			if(this.yFrom == 1) {//wir sind an start position
-				return ((diffy == 1 || diffy == 2) && this.xFrom == this.xTo);// bewegen wir uns 1 oder 2 NUR nach vorne?
+
+			return ((this.xFrom == this.xTo));// nicht start pos. wir können NUR EINS nach vorne
+
+		}else if(diffy == 2*offset1 ) {
+			if(this.yFrom == 1 + offset2) {//wir sind an start position
+				boolean keinerDazwischen = this.spielfeld[this.xFrom][this.yFrom+offset1] == 0;
+				return ((this.xFrom == this.xTo) && keinerDazwischen );// bewegen wir uns 1 oder 2 NUR nach vorne?
 			}
-			return (diffy == 1 && this.xFrom == this.xTo);// nicht start pos. wir können NUR EINS nach vorne
+			return false;
 		}
-		else {					//schwarz rest gilt 'umgekehrt' für restl fälle
-			if(Math.sqrt(Math.pow(diffx,2)) == 1 && diffy == -1) {
-				return (this.spielfeld[this.xTo][this.yTo] != 0);//ist dort ein gegner?
-			}
-			if(this.yFrom == 6) {
-				return ((diffy == -1 || diffy == -2) && this.xFrom == this.xTo);
-			}
-			return (diffy == -1 && this.xFrom == this.xTo);
-		}
-	}	
+		
+		return false;
+	
+	}
+	
 }
